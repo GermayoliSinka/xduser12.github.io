656666ffce25b105a32565fa502d6dcf8bd34d91
ERESSEA RS <rsilva@eresseasolutions.com>
2024-02-06 16:41:52 -0400

    Tests

---
+++ src/components/Examples/ButtonComponent/ButtonComponent.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import ButtonComponent from './ButtonComponent';
+import { describe, it, expect } from 'vitest';
+
+describe('ButtonComponent', () => {
+  it('renders a button', () => {
+    render(<ButtonComponent text="my button" />);
+    const button = screen.getByText('my button');
+    expect(button).toBeInTheDocument();
+  });
+});
