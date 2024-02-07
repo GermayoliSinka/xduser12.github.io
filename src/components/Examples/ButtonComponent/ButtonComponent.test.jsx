bb0068f0d8378734c9ffb1433de6aaf515dba198
ERESSEA RS <rsilva@eresseasolutions.com>
2024-02-07 17:04:03 -0400

    ProductTableComponent example component and test

--- src/components/Examples/ButtonComponent/ButtonComponent.test.jsx
+++ src/components/Examples/ButtonComponent/ButtonComponent.test.jsx
@@ -2,6 +2,9 @@ import { render, screen } from '@testing-library/react';
 import ButtonComponent from './ButtonComponent';
 import { describe, it, expect } from 'vitest';
 
+/**
+ * Test de renderizado de ButtonComponent
+ */
 describe('ButtonComponent', () => {
   it('renders a button', () => {
     render(<ButtonComponent text="my button" />);
